d9d8ad37644b1903bbbfa52e39e566d7fddd3db7
Stephen Niedzielski <stephen@niedzielski.com>
2015-03-14 22:41:57 -0600

    Add support for custom ticks.

--- spark-test.sh
+++ spark-test.sh
@@ -11,63 +11,63 @@ it_shows_help_with_no_argv() {
 it_graphs_argv_data() {
   graph="$($spark 1,5,22,13,5)"
 
-  test $graph = '_▁█▄▁'
+  test $graph = '▁▂█▅▂'
 }
 
 it_charts_pipe_data() {
   data="0,30,55,80,33,150"
   graph="$(echo $data | $spark)"
 
-  test $graph = '_▁▂▄▁█'
+  test $graph = '▁▂▃▄▂█'
 }
 
 it_charts_spaced_data() {
   data="0 30 55 80 33 150"
   graph="$($spark $data)"
 
-  test $graph = '_▁▂▄▁█'
+  test $graph = '▁▂▃▄▂█'
 }
 
 it_charts_way_spaced_data() {
   data="0 30               55 80 33     150"
   graph="$($spark $data)"
 
-  test $graph = '_▁▂▄▁█'
+  test $graph = '▁▂▃▄▂█'
 }
 
 it_handles_decimals() {
   data="5.5,20"
   graph="$($spark $data)"
 
-  test $graph = '_█'
+  test $graph = '▁█'
 }
 
 it_charts_100_lt_300() {
   data="1,2,3,4,100,5,10,20,50,300"
   graph="$($spark $data)"
 
-  test $graph = '____▂___▁█'
+  test $graph = '▁▁▁▁▃▁▁▁▂█'
 }
 
 it_charts_50_lt_100() {
   data="1,50,100"
   graph="$($spark $data)"
 
-  test $graph = '_▃█'
+  test $graph = '▁▄█'
 }
 
 it_charts_4_lt_8() {
   data="2,4,8"
   graph="$($spark $data)"
 
-  test $graph = '_▂█'
+  test $graph = '▁▃█'
 }
 
 it_charts_no_tier_0() {
   data="1,2,3,4,5"
   graph="$($spark $data)"
 
-  test $graph = '_▂▄▆█'
+  test $graph = '▁▂▄▆█'
 }
 
 it_equalizes_at_midtier_on_same_data() {
@@ -75,4 +75,16 @@ it_equalizes_at_midtier_on_same_data() {
   graph="$($spark $data)"
 
   test $graph = '▅▅▅▅'
+}
+
+it_ticks_custom() {
+  data="1,2,3,4,100,5,10,20,50,300"
+  graph="$(SPARK_TICKS='_▁▂▃▄▅▆▇█' $spark $data)"
+  test $graph = '____▂___▁█'
+}
+
+it_ticks_space() {
+  data="0,30,55,80,33,150"
+  graph="$(SPARK_TICKS=' ▁▂▃▄▅▆▇█' $spark $data)"
+  test "$graph" = ' ▁▂▄▁█'
 }
